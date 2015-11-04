1423849f6719f4e90bbe9de7a972bb2c8567d5fb
Jon Kuhl <jon-kuhl@uiowa.edu>
2015-11-04 16:17:04 +0000

    Initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  skip_before_filter :set_current_user
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      #sign in and redirect to show page
+      cookies.permanent[:session_token]= user.session_token
+      redirect_to movies_path
+    else
+      flash.now[:warning] = 'Invalid email/password combination'
+      render 'new'
+    end  
+  end
+
+  def destroy
+    cookies.delete(:session_token) 
+    @current_user=nil
+    flash[:notice]= 'You have logged out'
+    redirect_to movies_path
+  end
+
+end  
